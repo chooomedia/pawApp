02c9bec96bf5b5694e728e6ccfe80f85a08cd1fb
chooomedia <chooom@gmx.de>
2018-08-15 17:21:44 +0200

    first structure

--- controller/pawController.js
+++ controller/pawController.js
@@ -1,5 +1,79 @@
-var app = angular.module('pawApp',['ngMaterial', 'ngMessages']);
+angular
+    .module('pawApp', ['ngMaterial', 'ngMessages'])
+    .controller('pawController', function ($scope) {
+        $scope.title = "the Paw Site";
+        $scope.boxes = [
+            {
+                image: "http://www.placehold.it/180/180",
+                title: "Lorem Testtitle 1",
+                createDate: new Date(),
+                content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+                footer: "copyright 2018"
+            },
+            {
+                image: "http://www.placehold.it/180/180",
+                title: "Lorem Testtitle 4",
+                createDate: new Date(),
+                content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+                footer: "copyright 2018"
+            },
+            {
+                image: "http://www.placehold.it/180/180",
+                title: "Lorem Testtitle 3",
+                createDate: new Date(),
+                content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+                footer: "copyright 2018"
+            },
+            {
+                image: "http://www.placehold.it/180/180",
+                title: "Lorem Testtitle 3",
+                createDate: new Date(),
+                content:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+                footer: "copyright 2018"
+            },
+            {
+                image: "http://www.placehold.it/180/180",
+                title: "Lorem Testtitle 3",
+                createDate: new Date(),
+                content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+                footer: "copyright 2018"
+            },
+            {
+                image: "http://www.placehold.it/180/180",
+                title: "Lorem Testtitle 3",
+                createDate: new Date(),
+                content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+                footer: "copyright 2018"
+            }
+        ];
 
-app.controller('pawController', ['$scope', function($scope) {
-    $scope.title= "the Paw Site";
-}]);
+        $scope.links = [
+            {   title: "Home",
+                pathTo: "/"
+            },
+            {   title: "Bio",
+                step: "Über uns",
+                pathTo: "/" + this.title
+            },
+            {   title: "Album",
+                step: "Impressionen",
+                pathTo: "/" + this.title
+            },
+            {   title: "Karte",
+                step: "Wie sie uns finden",
+                pathTo: "/" + this.title
+            }
+        ]
+    })
+    .config(function($mdThemingProvider) {
+
+        $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+        $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
+        $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
+        $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
+    })
+    .config(function($mdIconProvider) {
+        $mdIconProvider
+            .iconSet('social', 'img/icons/sets/social-icons.svg', 24)
+            .defaultIconSet('img/icons/sets/core-icons.svg', 24);
+    });
