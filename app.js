7f46fa30e475246d066d3e8fd845342c1e2fa8d5
Chris Matt <christopher@chooo.media>
2018-08-17 14:48:19 +0200

    first service inside controller :-)

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var app = angular.module('pawApp', ['ngMaterial', 'ngMessages', 'ngAnimate'])
-  .config(function($mdThemingProvider) {
-    $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
-    $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
-    $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
-    $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
-  });
+    .config(function ($mdThemingProvider) {
+        $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+        $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
+        $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
+        $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
+    });
 
--- controllers/pawController.js
+++ controllers/pawController.js
@@ -1,46 +1,58 @@
-app.controller('pawController', ['$scope', '$http', function ($scope, $http) {
+app.controller('pawController', ['$scope', '$http', 'pawService', function ($scope, $http, pawService) {
 
-  $scope.logo =
-    {
-      url: 'paw.logo.png',
-      title: 'the Paw Logo',
-      alt: 'Logo 2018'
-    };
+    $scope.data = pawService('Suche');
 
-  $http({
-    method: 'GET',
-    url: 'data/pawContent.json'
-  }).then(function (response) {
-    $scope.boxes = response.data;
-  }, function (error) {
-    console.log(error);
-  }, 2000);
+    $scope.logo =
+        {
+            url: 'paw.logo.png',
+            title: 'the Paw Logo',
+            alt: 'Logo 2018'
+        };
 
-  $scope.title = "the Paw Site";
-  $scope.links = [
-    {
-      title: "Home",
-      pathTo: "/"
-    },
-    {
-      title: "Bio",
-      step: "Über uns",
-      pathTo: "/" + this.title
-    },
-    {
-      title: "Album",
-      step: "Impressionen",
-      pathTo: "/" + this.title
-    },
-    {
-      title: "Karte",
-      step: "Wie sie uns finden",
-      pathTo: "/" + this.title
-    }
-  ]
+    $http({
+        method: 'GET',
+        url: 'data/pawContent.json'
+    }).then(function (response) {
+        $scope.boxes = response.data;
+    }, function (error) {
+        console.log(error);
+    }, 2000);
+
+    $scope.title = "the Paw Site";
+    $scope.links = [
+        {
+            title: "Home",
+            pathTo: "/"
+        },
+        {
+            title: "Bio",
+            step: "Über uns",
+            pathTo: "/" + this.title
+        },
+        {
+            title: "Album",
+            step: "Impressionen",
+            pathTo: "/" + this.title
+        },
+        {
+            title: "Karte",
+            step: "Wie sie uns finden",
+            pathTo: "/" + this.title
+        }
+    ]
 }])
-  .config(function ($mdIconProvider) {
-    $mdIconProvider
-      .iconSet('social', 'img/icons/sets/social-icons.svg', 24)
-      .defaultIconSet('img/icons/sets/core-icons.svg', 24);
-  });
+    .config(function ($mdIconProvider) {
+        $mdIconProvider
+            .iconSet('social', 'img/icons/sets/social-icons.svg', 24)
+            .defaultIconSet('img/icons/sets/core-icons.svg', 24);
+    })
+
+    .factory('pawService', function () {
+        return function (data) {
+            if (data.length >= 3) {
+                return data;
+            } else {
+                console.log('Dein Eintrag ist zu kurz');
+            }
+        }
+    });
